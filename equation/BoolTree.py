5c3ca33442d08608c3422a5018e2d3ae365121a8
Andrew Barnes <06abarne@googlemail.com>
2017-02-16 16:02:13 +0000

    examples added and blind algorithm.

--- equation/BoolTree.py
+++ equation/BoolTree.py
@@ -1,7 +1,7 @@
 """ This module contains the classes required to make a boolean equation. """
 from abc import ABC, abstractmethod
 
-VARIABLES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+VARIABLES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 class BooleanNode(ABC):
     """ An abstract node which contains everything required to create a tree. """
