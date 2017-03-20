ebd28207136f8d2a2f1acb1fd1395130313ecda2
Andrew Barnes <06abarne@googlemail.com>
2017-03-20 23:03:33 +0000

    Added unit tests for binary genome.

--- PartyProblemSimulator/Solvers/Organisms/BinaryGenome.py
+++ PartyProblemSimulator/Solvers/Organisms/BinaryGenome.py
@@ -4,15 +4,15 @@ from random import randint
 class BinaryGenome(Genome):
     """ A binary encoding of a genome consisting of bits. """
 
-    def __init__(self, var_count):
-        """ Initialises the number of variables required in this genome. """
-        self._var_count = var
-        Genome.__init__()
+    def __init__(self, gene_count):
+        """ Initialises the number of genes required in this genome. """
+        self._set_gene_count(gene_count)
+        Genome.__init__(self)
 
     def _instantiate(self):
         """ Initialises this genome with a random set of binary values. """
         count = 0
-        while count < self._get_var_count():
+        while count < self._get_gene_count():
             if randint(0, 100) < 50:        # Randomize the value to apply to the gene
                 self.add_gene(False)
             else:
@@ -24,20 +24,20 @@ class BinaryGenome(Genome):
         input_vector = {}
         count = 0   # Generate the input vector
         for gene in self.get_genes():
-            var_id = "{" + str(count) + "}" # Generate the identifier for this variable in the equation.
-            input_vector[var_id] = gene
+            gene_id = "{" + str(count) + "}" # Generate the identifier for this gene in the equation.
+            input_vector[gene_id] = gene
             count = count + 1
         clausal_result = equation.get_clause_evaluation(input_vector)
         score = sum(clausal_result) / len(clausal_result)   # Calculate the percentage of clauses met.
         return score
 
-    def _set_var_count(self, var_count):
-        """ Safely sets the var count of this genome. """
-        if var_count > 0:
-            self._var_count = var_count
+    def _set_gene_count(self, gene_count):
+        """ Safely sets the gene count of this genome. """
+        if gene_count > 0:
+            self._gene_count = gene_count
         else:
-            self._var_count = 1
+            self._gene_count = 1
     
-    def _get_var_count(self):
-        """ Retrieves the variable count for this binary genome. """
-        return self._var_count
+    def _get_gene_count(self):
+        """ Retrieves the gene count for this binary genome. """
+        return self._gene_count
