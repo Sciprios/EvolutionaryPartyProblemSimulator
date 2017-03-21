f22b6d5ebb5b1ffa2d399d4e39ebba3ec11b9609
Andrew Barnes <06abarne@googlemail.com>
2017-03-21 21:59:26 +0000

    Minor unittests added for genetic algorithm.

--- PartyProblemSimulator/Solvers/GeneticAlgorithm.py
+++ PartyProblemSimulator/Solvers/GeneticAlgorithm.py
@@ -1,16 +1,19 @@
- from PartyProblemSimulator.Observation.Subject import Subject
+from PartyProblemSimulator.Observation.Subject import Subject
 
 class GeneticAlgorithm(Subject):
     """ A genetic algorithm evolves a solution to a problem. """
     
-    def __init__(self):
+    def __init__(self, max_generations):
         """ Initializes the instance level variables. """
-        self._population = []
-        self._generation = 0
         self._eval_count = 0
-        Subject.__init__()
+        self._repopulate([])    # Empty population
+        self._set_generation(0)
+        self._set_equation(None)
+        self._set_best_genome(None)
+        self._set_max_generation(max_generations)
+        Subject.__init__(self)
     
-    def notify_observers(self):
+    def _notify_observers(self):
         """ Notifies observers on completion of a generation with the best organism and current stats. """
         # Arguments to be passed to each observer
         args = {
@@ -21,18 +24,91 @@ class GeneticAlgorithm(Subject):
         for obs in self._observers:
             obs.update(args)
     
-    def _get_best_genome(self):
-        """ Retrieves the best genome from the population. """
-        pass
+    def run(self, equation):  # pragma: no cover
+        """ Runs the genetic algorithm on the given equation. """
+        self._set_generation(0) # Reset algorithm attributes
+        self._reset_eval_count()
+        self._set_equation(equation)
+        self._initialise()  # Initialize a population
+        while (self.get_generation() <= self.get_max_generation()) and (self._get_best_genome().evaluate(equation) != 1):
+            self._set_generation(self.get_generation() + 1)
+            self._evolve()
+            self._notify_observers()
     
-    def get_population(self):
+    def _initialise(self): # pragma: no cover
+        """ Initializes the population of organisms. """
+        raise NotImplementedError("The initialise method has not been implemented by the base class {}".format(type(self)))
+    
+    def _evolve(self): # pragma: no cover
+        """ Evolves the instance's population through a single generation. """
+        new_population = []
+        parents = self._selection()
+        new_population.extend(self._reproduction(parents))
+        self._mutation(new_population)
+        self._repopulate(new_population)
+    
+    def _selection(self): # pragma: no cover
+        """ Selection of parents from the population and identifies the best genome. """
+        raise NotImplementedError("The _selection method has not been inherited by the base class {}".format(type(self)))
+
+    def _reproduction(self, parents): # pragma: no cover
+        """ Reproduces children based on the selected parents. """
+        raise NotImplementedError("The _reproduction method has not been inherited by the base class {}".format(type(self)))
+
+    def _mutation(self, new_population): # pragma: no cover
+        """ Mutates the new population. """
+        raise NotImplementedError("The _mutation method has not been inherited by the base class {}".format(type(self)))
+
+    def _repopulate(self, new_population): # pragma: no cover
+        """ Repopulates the population of this genetic algorithm. """
+        self._population = new_population
+
+    def _set_generation(self, gen): # pragma: no cover
+        """ Sets the generation. """
+        if gen >= 0 :
+            self._generation = gen
+        else:
+            self._generation = 0
+    
+    def _set_best_genome(self, best): # pragma: no cover
+        """ Safely sets the best genome. """
+        self._best_genome = best
+
+    def _set_equation(self, equation): # pragma: no cover
+        """ Safely sets the equation attribute of this algorithm. """
+        self._equation = equation
+    
+    def _increment_eval_count(self): # pragma: no cover
+        """ Increments the number of evaluations. """
+        self._eval_count = self._eval_count + 1
+    
+    def _set_max_generation(self, max): # pragma: no cover
+        """ Sets the max generation of the algorithm. """
+        if max > 0:
+            self._max_generation = max
+        else:
+            self._max_generation = 1
+
+    def _reset_eval_count(self): # pragma: no cover
+        """ Resets the evaluation count to 0. """
+        self._eval_count = 0
+
+    def _get_best_genome(self): # pragma: no cover
+        """ Retrieves the best genome from the population. """
+        return self._best_genome
+
+    def get_max_generation(self): # pragma: no cover
+        """ Retrieves the maximum generation. """
+        return self._max_generation
+
+    def get_population(self): # pragma: no cover
         """ Retrieves the population. """
         return self._population
 
-    def get_generation(self):
+    def get_generation(self): # pragma: no cover
         """ Retrieves the current generation this algorithm is on. """
         return self._generation
     
-    def get_num_evaluations(self):
+    def get_num_evaluations(self): # pragma: no cover
         """ Retrieves the number of evaluations this method has used. """
         return self._eval_count
---
+++ PartyProblemSimulator/Solvers/unit_tests/test_geneticalgorithm.py
@@ -0,0 +1,21 @@
+from unittest.mock import MagicMock, Mock, patch
+from unittest import TestCase
+from PartyProblemSimulator.Solvers.GeneticAlgorithm import GeneticAlgorithm
+
+class TestGeneticAlgorithm(TestCase):
+    """ Tests the genetic algorithm class. """
+
+    @patch('PartyProblemSimulator.Solvers.GeneticAlgorithm.GeneticAlgorithm._set_max_generation')
+    @patch('PartyProblemSimulator.Solvers.GeneticAlgorithm.GeneticAlgorithm._set_best_genome')
+    @patch('PartyProblemSimulator.Solvers.GeneticAlgorithm.GeneticAlgorithm._set_equation')
+    @patch('PartyProblemSimulator.Solvers.GeneticAlgorithm.GeneticAlgorithm._set_generation')
+    @patch('PartyProblemSimulator.Solvers.GeneticAlgorithm.GeneticAlgorithm._repopulate')
+    def test_init(self, repop, set_gen, set_eq, set_genome, set_max):
+        """ Ensures the init instantiates a genetic algorithm correctly. """
+        max_generations = Mock()
+        ga = GeneticAlgorithm(max_generations)
+        set_gen.assert_called_with(0)   # Check everything has been called.
+        set_eq.assert_called_with(None)
+        set_genome.assert_called_with(None)
+        set_max.assert_called_with(max_generations)
+        repop.assert_called_with([])
