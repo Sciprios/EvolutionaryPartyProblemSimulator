bbbab7ee4fe62a6ff41a6b46b8a79c74569f2153
Andrew Barnes <06abarne@googlemail.com>
2017-04-01 08:50:03 +0100

    SatComparisor added (UNFINISHED)

--- PartyProblemSimulator/Experiments/Experiment.py
+++ PartyProblemSimulator/Experiments/Experiment.py
@@ -5,13 +5,13 @@ class Experiment(Thread):
 
     def run(self):
         """ Should be implemented to execute the experiment and save results. """
-        results = self.do_experiment()
-        self.save_results(results)
+        results = self._do_experiment()
+        self._save_results(results)
     
-    def do_experiment(self):
+    def _do_experiment(self):
         """ Execute the experiment and return results. """
         raise NotImplementedError("The do_experiment method of Experiment is not implemented.")
     
-    def save_results(self, results):
+    def _save_results(self, results):
         """ Saves the results of this experiment to disk. """
         raise NotImplementedError("The save_results method of Experiment is not implemented.")
