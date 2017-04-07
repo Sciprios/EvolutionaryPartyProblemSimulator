b3849bba910d46563c2956242f72485fd824923c
Andrew Barnes <06abarne@googlemail.com>
2017-04-07 08:26:02 +0100

    Refactoring of abstract methods.

--- PartyProblemSimulator/Experiments/Experiment.py
+++ PartyProblemSimulator/Experiments/Experiment.py
@@ -23,3 +23,26 @@ class Experiment(Thread):
     def _save_results(self, results):
         """ Saves the results of this experiment to disk. """
         raise NotImplementedError("The save_results method of Experiment is not implemented.")
+
+    def _load_test_cases(self, results):
+        """ Loads or creates the test cases to be used. """
+        raise NotImplementedError("The _load_test_cases method of Experiment is not implemented.")
+
+    def _test_method(self, method, no_trials, test_cases):
+        """ Tests the given method with x trials on all test cases provided. """
+        raise NotImplementedError("The _test_method method of Experiment is not implemented.")
+
+    def _do_trial(self, method, equation, variable_count):
+        """ Does a single trial of the algorithm provided. """
+        raise NotImplementedError("The _do_trial method of Experiment is not implemented.")
+
+    def _calculate_results(self, results):
+        """ Calculates the SR (Success Rate) and AES (Average Evaluations per Solution) based on the results given."""
+        sr = 0
+        aes = 0
+        for result in method_results:
+            aes = aes + result['AES']
+            sr = sr + result['SR']
+        aes = aes / len(method_results)
+        sr = sr / len(method_results)
+        return {"AES": aes, "SR": sr}
